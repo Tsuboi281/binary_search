e55312a38a1d89a3d05b68fef701440097f19ea7
Tsuboi Shusuke <tsuboi.s.ab@is-mac20.edu.c.titech.ac.jp>
2019-06-18 16:58:38 +0900

    test

--- src/array.c
+++ src/array.c
@@ -11,8 +11,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  int lb = -1;
+  int ub = n;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(A[m] >= x) ub = m;
+    else lb = m;
+    }
+    printf("%d\n" ,ub);
   return 0;
 }
