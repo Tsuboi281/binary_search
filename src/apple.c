5c1fb9675b6a0f327989aafb2dc4ed4fdd3c6c52
Tsuboi Shusuke <tsuboi.s.ab@is-mac40.edu.c.titech.ac.jp>
2019-06-21 15:44:11 +0900

    apple.c

--- src/apple.c
+++ src/apple.c
@@ -11,7 +11,24 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-  return 0;
+  int j = 0;
+  lb = 0;
+  ub = A[0];
+  while(j < n){
+    j += 1;
+    if (ub < A[j]) ub = A[j];
+    }//Aの要素の最大値はりんごバックに入れることができるリンゴの個数としてあり得るのでそれをubとしてとる
+  while (ub - lb > 1){
+    int m = (lb + ub) / 2;
+    int i = 0;
+    int w =  0;
+    while (i < n){
+        w += ((A[i] -1)/ m) + 1;
+        i += 1;
+    }
+    if  (w <= k) ub = m;
+    else lb = m;
+    }//Σ(a_i/mの余りの切り上げ) <= kを満たす最小のmが求めるm
+printf("%d\n" ,ub);
+return 0;
 }
