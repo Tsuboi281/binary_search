560087208d63e7e46f0ac491c9f104b648aa15ce
Tsuboi Shusuke <tsuboi.s.ab@is-mac33.edu.c.titech.ac.jp>
2019-06-24 17:56:40 +0900

    spear.cを訂正しました

--- src/spear.c
+++ src/spear.c
@@ -12,21 +12,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  int j = 0;
+  int j;
   lb = 0;//作ることができる槍の長さの最小値
   long long int x = A[0];
-  while(j < n){
-    j += 1;
+  for(j = 0;j < n;j++){
     x += A[j];
     }
     ub = (x / k)+1;//この値だと必ずk本の槍を作ることができない
   while (ub - lb > 1){
     int m = (lb + ub) / 2;
-    int i = 0;
+    int l;
     int w =  0;
-    while (i < n){
-        w = w + (A[i]/ m);
-        i += 1;
+    for(l = 0;l < n;l++){
+        w = w + (A[l]/ m);
     }
     if  (w >= k) lb = m;
     else ub = m;
