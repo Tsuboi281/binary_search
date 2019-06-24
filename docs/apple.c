4ad8cdbe4d2bee9de4f6c000d5b0899905d5c12e
Tsuboi Shusuke <tsuboi.s.ab@is-mac33.edu.c.titech.ac.jp>
2019-06-24 18:24:57 +0900

    apple.cを訂正しました

---
+++ docs/apple.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int n;
+int k;
+int A[100000];
+
+
+int main(){
+  int i, lb, ub;
+  scanf("%d%d", &n, &k);
+  for(i = 0; i < n; i++){
+    scanf("%d", &A[i]);
+  }
+  int j;
+  lb = 0;
+  ub = A[0];
+  for(j = 1;j < n; j ++){
+    if (ub < A[j]) ub = A[j];
+    }//Aの要素の最大値はりんごバックに入れることができるリンゴの個数としてあり得るのでそれをubとしてとる
+  while (ub - lb > 1){
+    int m = (lb + ub) / 2;
+    int l;
+    int w =  0;
+    for (l = 0; l < n; l++){
+        w += ((A[l] -1)/ m) + 1;
+    }
+    if  (w <= k) ub = m;
+    else lb = m;
+    }//Σ(a_i/mの余りの切り上げ) <= kを満たす最小のmが求めるm
+printf("%d\n" ,ub);
+return 0;
+}
