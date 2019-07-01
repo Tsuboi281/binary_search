f3e28e16e65842ea3dc6d181376840c32395803f
Tsuboi281 <shusuke281@gmail.com>
2019-07-01 17:11:17 +0900

    spear.cを訂正しました

--- src/spear.c
+++ src/spear.c
@@ -7,28 +7,30 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i;
+  long long int ub,lb;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
   int j;
   lb = 0;//作ることができる槍の長さの最小値
-  long long int x = A[0];
-  for(j = 0;j < n;j++){
-    x += A[j];
+  ub = A[0];
+  for(j = 1;j < n;j++){
+    ub += A[j];
     }
-    ub = (x / k)+1;//この値だと必ずk本の槍を作ることができない
+    ub = (ub / k)+1;//この値だと必ずk本の槍を作ることができない
+    long long int m;
+    int l,w;
   while (ub - lb > 1){
-    int m = (lb + ub) / 2;
-    int l;
-    int w =  0;
+    m = (lb + ub) / 2;
+    w =  0;
     for(l = 0;l < n;l++){
         w = w + (A[l]/ m);
     }
     if  (w >= k) lb = m;
     else ub = m;
     }
-printf("%d\n" ,lb);
+printf("%lld\n" ,lb);
 return 0;
 }
